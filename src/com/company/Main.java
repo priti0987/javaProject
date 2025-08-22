2cd8716ea026f3ca204f8048632f2ebe3e01d854
priti <pritibhushanf@gmail.com>
2025-08-22 09:44:14 +0530

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Priti");
+
+        for (int i=0;i<=5;i++)
+        {
+            System.out.println("i is "+i);
+        }
+    }
+}
---
+++ src/com/company/swap2Numbers.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class swap2Numbers {
+    public static void main(String[] args) {
+        System.out.println("swap 2 Numbers");
+    }
+
+}
